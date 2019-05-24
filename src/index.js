274ed623abaac45b9839f52f2bb1fc19bda31756
Assisneto <netodamasceno12@gmail.com>
2019-05-24 16:19:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => <View />;
+
+export default App;
