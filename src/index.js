53bab5c338830b660b4584d0b6b3f47c496e3045
Assisneto <netodamasceno12@gmail.com>
2019-05-27 21:12:18 -0300

    Initial Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
 import './config/ReactotroConfig';
-import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import Routes from './routes';
+import createNavigator from './routes';
 
-const App = () => <Routes />;
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
 
-export default App;
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    this.setState({
+      userChecked: true,
+      userLogged: !!username,
+    });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+    return <Routes />;
+  }
+}
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
-const Repositories = () => <View />;
+const Repositories = () => (
+  <View>
+    <Text>teste</Text>
+  </View>
+);
 
 export default Repositories;
